d7814b24f9745473797fcce9da9fb8e1d6f549eb
Matanaveh123 <85345183+Matanaveh123@users.noreply.github.com>
2022-05-28 16:41:45 +0300

    2

---
+++ Flights Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public Material blue;
+    public Animator transition;
+    public TMP_Text scoreText;
+
+    public int ringsLeft = 3;
+    int score = 0;
+    
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        scoreText.text = score.ToString();
+
+        /// Give player boost at the start
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            GetComponent<Rigidbody>().velocity = transform.forward * 150;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /// If touching the ground, restart
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ring") && other.gameObject.GetComponent<Ring>().isTaken == false)
+        {
+            ///Play collected sound
+            other.gameObject.GetComponent<AudioSource>().Play();
+
+            score++;
+            scoreText.text = score.ToString();
+
+            ringsLeft--;
+            other.gameObject.GetComponent<MeshRenderer>().material = blue;
+        }
+        else if(other.gameObject.CompareTag("SpecialRing") && other.gameObject.GetComponent<Ring>().isTaken == false)
+        {
+            if(ringsLeft == 1)
+            {
+                TransitionToNextLevel(other);
+            }
+        }
+    }
+
+    private void TransitionToNextLevel(Collider other)
+    {
+        other.gameObject.GetComponent<AudioSource>().Play();
+
+        score++;
+        scoreText.text = score.ToString();
+
+        ringsLeft--;
+        other.gameObject.GetComponent<MeshRenderer>().material = blue;
+        if(SceneManager.GetActiveScene().buildIndex != 0) { transition.SetTrigger("End"); }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
